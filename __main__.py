f6ff7139ff369d2fe62f70c886bc69bbfd8e7000
Nepy0x <nepy0x@proton.me>
2026-02-15 18:57:29 +0330

    Add byte count

--- __main__.py
+++ __main__.py
@@ -1,5 +1,70 @@
-def main():
-    print("Hello")
+from argparse import ArgumentParser, RawTextHelpFormatter
+from sys import exit as sys_exit, stdin
+
+
+class Details:
+    byte_count = 0
+
+
+def main() -> None:
+    progname = "pywc"
+    parser = ArgumentParser(
+        progname,
+        f"{progname} [OPTION]... [FILE]...\r\n{progname} [OPTION]... --files0-from=F",
+        "Print  newline,  word, and byte counts for each FILE, and a total line if more than one FILE is specified.\r\n"
+            "A word is a nonempty sequence of non white space delimited by white space characters or by start or end of input.\n\n"
+            "With no FILE, or when FILE is -, read standard input.\n\n"
+            "The options below may be used to select which counts are printed, always in the following order: newline, word, character, byte, maximum line length.",
+        formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument("FILE", type=str, nargs="*", help="The file in question")
+    parser.add_argument("-c", "--bytes", action="store_true", help="Print the byte counts")
+    
+    args = parser.parse_args()
+    
+    # No FILE
+    if not args.FILE:
+        details = get_stdin_details()
+        print(details.byte_count)
+
+    # FILE provided
+    for path in args.FILE:
+        if path == "-":
+            details = get_stdin_details()
+        else:
+            try:
+                details = get_file_details(path)
+            except FileNotFoundError:
+                print(f"{parser.prog}: {path}: No such file or directory")
+                sys_exit(1)
+            except IsADirectoryError:
+                print(f"{parser.prog}: {path}: Is a directory")
+                sys_exit(1)
+            except PermissionError:
+                print(f"{parser.prog}: {path}: Permission denied")
+                sys_exit(1)
+        print(details.byte_count)
+
+
+def get_file_details(path:str) -> Details:
+    details = Details()
+    with open(path, "r") as file:
+        for line in file:
+            for char in line:
+                # BYTES
+                details.byte_count += len(char.encode("utf-8"))
+    
+    return details
+
+
+def get_stdin_details() -> Details:
+    details = Details()
+    data = stdin.buffer.read()
+    # BYTES
+    details.byte_count += len(data)
+
+    return details
+
 
 if __name__ == "__main__":
     main()
