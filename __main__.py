eae28e4651702c9ce1d7ad2d159fb534fbb5827f
Nepy0x <nepy0x@proton.me>
2026-02-15 22:31:42 +0330

    Add input/output methods

--- __main__.py
+++ __main__.py
@@ -1,11 +1,18 @@
 from argparse import ArgumentParser, RawTextHelpFormatter
 from sys import exit as sys_exit, stdin
+from enum import Enum
 
 
 class Details:
     byte_count = 0
     char_count = 0
     newline_count = 0
+
+
+class State(Enum):
+    LINES = 1
+    BYTES = 4
+    CHARS = 8
 
 
 def main() -> None:
@@ -23,17 +30,20 @@ def main() -> None:
     parser.add_argument("-c", "--bytes", action="store_true", help="Print the byte counts")
     parser.add_argument("-m", "--chars", action="store_true", help="Print the character counts")
     parser.add_argument("-l", "--lines", action="store_true", help="Print the newline counts")
+    parser.add_argument("--files0-from", type=str, help="Read input from the files specified by NUL-terminated names in file F; If F is - then read names from standard input")
     
     args = parser.parse_args()
+
+    state = get_state(args)
     
     # No FILE
-    if not args.FILE:
+    if not args.FILE + (get_files0_from(args.files0_from) if args.files0_from else []):
         details = get_stdin_details()
-        print(details.newline_count)
+        print_report(state, details)
         sys_exit(0)
 
     # FILE provided
-    for path in args.FILE:
+    for path in args.FILE + (get_files0_from(args.files0_from) if args.files0_from else []):
         if path == "-":
             details = get_stdin_details()
         else:
@@ -48,7 +58,7 @@ def main() -> None:
             except PermissionError:
                 print(f"{parser.prog}: {path}: Permission denied")
                 sys_exit(1)
-        print(details.newline_count)
+        print_report(state, details, path)
 
 
 def get_file_details(path:str) -> Details:
@@ -80,6 +90,30 @@ def get_stdin_details() -> Details:
             details.newline_count += 1
 
     return details
+
+
+def get_files0_from(path) -> list[str]:
+    with open(path, "r") as file:
+        text = file.read()
+        return [x for x in text.split("\0")]
+
+
+def get_state(args) -> int:
+    state = 0
+    state |= args.lines * State.LINES.value
+    state |= args.chars * State.CHARS.value
+    state |= args.bytes * State.BYTES.value
+    return state or State.CHARS.value | State.LINES.value | State.BYTES.value # TODO: change to line, word and byte
+
+
+def print_report(state, details, path="") -> None:
+    if state & State.LINES.value:
+        print(f"{details.newline_count:4}", end="  ")
+    if state & State.CHARS.value:
+        print(f"{details.char_count:4}", end="  ")
+    if state & State.BYTES.value:
+        print(f"{details.byte_count:4}", end="  ")
+    print(path)
 
 
 if __name__ == "__main__":
